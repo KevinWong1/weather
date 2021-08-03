8bdcd8af0992daf4bc469e288e30fe5fa480d3c0
kevinwong1 <kevinwng0@gmail.com>
2021-08-02 20:04:38 -0700

    blah

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+import requests
+import datetime
+import os
 
 
 def index(request):
-    return render(request, 'app1/index.html')
+
+    OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+    api_key = os.environ.get("OWM_API_KEY")
+    lat = "37.804829"
+    lon = "-122.272476"
+    time = datetime.datetime.now()
+    weather_params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": api_key,
+        "exculde": "current,minutely,daily"
+    }
+
+    response = requests.get(OWM_Endpoint, params=weather_params)
+    weather_data = response.json()
+    context = {
+        "weather": weather_data["current"]["weather"][0]["main"],
+        "time": time
+    }
+    return render(request, 'app1/index.html', context)
