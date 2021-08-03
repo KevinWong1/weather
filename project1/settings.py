bbf2c598030ded010e10cec4b2c201a3fb06498b
kevinwong1 <kevinwng0@gmail.com>
2021-08-02 20:30:13 -0700

    blah

--- project1/settings.py
+++ project1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%qq28n0d20yuamuyv0@r(un2cgp*jjw6)-0io02ai-)qqd$d5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
